971efe19e1a21f80789dc6533accd94960be2704
IlMelons <melonss2002@gmail.com>
2025-12-20 19:16:22 +0100

    Added fxmanifest

---
+++ fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+
+name 'mnr_core'
+description 'Monarch Devs Framework'
+author 'IlMelons'
+version '0.0.1'
+repository 'https://github.com/Monarch-Devs/mnr_core'
+
+files {}
+
+shared_scripts {}
+
+client_scripts {}
+
+server_scripts {}
