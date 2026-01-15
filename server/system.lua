6d89f6e4a8a0469c421833f1af2dfc4d7e6adc56
IlMelons <melonss2002@gmail.com>
2026-01-15 10:19:49 +0100

    Beta system initialization

--- server/system.lua
+++ server/system.lua
@@ -1,1 +1,17 @@
----@todo System Initialization
+SystemReady = false
+local start = os.time()
+
+local function checkSystemReady()
+    if os.time() - start >= 60 and not SystemReady then
+        SystemReady = true
+        ---@todo [OPTIONAL] Event trigger to notify other scripts that the system is ready
+        -- TriggerEvent('mnr:SystemReady')
+    end
+end
+
+AddEventHandler('onResourceStart', function(resourceName)
+    start = os.time()
+    SetTimeout(60000, checkSystemReady)
+end)
+
+SetTimeout(60000, checkSystemReady)
