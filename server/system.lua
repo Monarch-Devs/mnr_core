d704a7873d17d5d2fc541d597a4e5160ca98ee96
IlMelons <melonss2002@gmail.com>
2026-04-01 19:15:24 +0200

    feat(system): add rename ensurer

--- server/system.lua
+++ server/system.lua
@@ -15,3 +15,13 @@ AddEventHandler('onResourceStart', function(resourceName)
 end)
 
 SetTimeout(60000, checkSystemReady)
+
+AddEventHandler('onResourceStart', function(name)
+    if GetCurrentResourceName() ~= name then
+        return
+    end
+
+    if GetResourceMetadata(name, 'name', 0) ~= name then
+        error('[MONARCH] Wrong resource name detected, fix it', 0)
+    end
+end)
