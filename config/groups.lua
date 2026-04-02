09ec88856547c1eb77f3bf100443760d7e548a14
IlMelons <melonss2002@gmail.com>
2026-04-02 14:21:44 +0200

    chore(groups): add example groups

---
+++ config/groups.lua
@@ -0,0 +1,51 @@
+return {
+    ['civilian'] = {
+        label = 'Civilian',
+        cat = 'CIV'
+        grades = {
+            [1] = { label = 'Unemployed' },
+        },
+    },
+    ['lspd'] = {
+        label = 'LSPD',
+        cat = 'GOV',
+        grades = {
+            [1] = { label = 'Police Officer I' },
+            [2] = { label = 'Police Officer II' },
+            [3] = { label = 'Police Officer III' },
+            [4] = { label = 'Detective' },
+            [5] = { label = 'Sergeant I' },
+            [6] = { label = 'Sergeant II' },
+            [7] = { label = 'Lieutenant I' },
+            [8] = { label = 'Lieutenant II' },
+            [9] = { label = 'Captain I' },
+            [10] = { label = 'Captain II' },
+            [11] = { label = 'Captain III' },
+            [12] = { label = 'Commander' },
+            [13] = { label = 'Deputy Chief' },
+            [14] = { label = 'Assistant Chief' },
+            [15] = { label = 'Chief' },
+        },
+    },
+    ['lsmd'] = {
+        label = 'LSMD',
+        cat = 'EMS',
+        grades = {
+            [1] = { label = 'Observer' },
+            [2] = { label = 'Medical Student I' },
+            [3] = { label = 'Medical Student II' },
+            [4] = { label = 'Intern' },
+            [5] = { label = 'Junior Resident' },
+            [6] = { label = 'Resident' },
+            [7] = { label = 'Senior Resident' },
+            [8] = { label = 'Chief Resident' },
+            [9] = { label = 'Fellow' },
+            [10] = { label = 'Attending Physician' },
+            [11] = { label = 'Senior Attending' },
+            [12] = { label = 'Specialist / Surgeon' },
+            [13] = { label = 'Senior Surgeon' },
+            [14] = { label = 'Head of Department' },
+            [15] = { label = 'Medical Director' },
+        },
+    },
+}
