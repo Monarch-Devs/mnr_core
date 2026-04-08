9cbda5e46091a10bb7706c0b3b5f3811dbafcc5b
IlMelons <melonss2002@gmail.com>
2026-04-08 22:48:55 +0200

    chore(groups): add class constructor

--- server/groups/class.lua
+++ server/groups/class.lua
@@ -1,3 +1,14 @@
--- [PLACEHOLDER] GROUP CLASS SYSTEM
 local MnrGroup = {}
 MnrGroup.__index = MnrGroup
+
+function MnrGroup.new(name, cat)
+    return setmetatable({
+        name = name,
+        cat = cat,
+        duty = {},
+        online = {},
+        offline = {},
+    }, MnrPlayer)
+end
+
+return MnrGroup
